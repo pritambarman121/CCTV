c97f8ea5d6ee6f83bccb0b01e695848837a23a24
pritambarman121 <49247449+pritambarman121@users.noreply.github.com>
2022-02-19 12:06:15 +0530

    new

--- main.py
+++ main.py
@@ -7,43 +7,43 @@ from record import record
 window = Tk()
 window.title("CCTV")
 window.iconphoto(False,PhotoImage(file='icon1.png'))
-window.geometry('720x540')
+window.geometry('1080x240')
 
 #main frame
 mainFrame = Frame(window,bd=2)
 
-label_title = Label(mainFrame,text = "CCTV CAMERA",font=('Helvitica',40,'bold'))
+label_title = Label(mainFrame,text = "CCTV       AMERA",font=('Helvitica',40,'bold'))
 label_title.grid(pady=(10,10),column=1)   #title 
 
 icon_1=Image.open('icons/icon1.png')
 icon_1=icon_1.resize((50,50),Image.ANTIALIAS)
 icon_1=ImageTk.PhotoImage(icon_1)
 label_icon_1=Label(mainFrame,image=icon_1)
-label_icon_1.grid(row=0,pady=(5,10),column=0)   #title icon
+label_icon_1.grid(row=0,pady=(5,10),column=1)   #title icon
 
 #record btn
 btn_image=Image.open('icons/recording.png')
 btn_image=btn_image.resize((60,60),Image.ANTIALIAS)
 btn_image=ImageTk.PhotoImage(btn_image)
 
-btn=Button(mainFrame,font=('Helvitica',20,'bold'),height=90,width=170,image=btn_image,compound='left',command=record)
-btn.grid(row=2,pady=(20,10),column=1)
+btn=Button(mainFrame,font=('Helvitica',20,'bold'),height=90,width=200,image=btn_image,compound='left',command=record)
+btn.grid(row=1,pady=(20,10),column=0)
 
 #file btn
 btn_image1=Image.open('icons/File.png')
 btn_image1=btn_image1.resize((60,60),Image.ANTIALIAS)
 btn_image1=ImageTk.PhotoImage(btn_image1)
 
-btn=Button(mainFrame,font=('Helvitica',20,'bold'),height=90,width=170,image=btn_image1,compound='left')
-btn.grid(row=3,pady=(10,10),column=1)
+btn=Button(mainFrame,font=('Helvitica',20,'bold'),height=90,width=200,image=btn_image1,compound='left')
+btn.grid(row=1,pady=(10,10),column=1)
 
 #exit btn
 btn_image2=Image.open('icons/exit.png')
 btn_image2=btn_image2.resize((60,60),Image.ANTIALIAS)
 btn_image2=ImageTk.PhotoImage(btn_image2)
 
-btn=Button(mainFrame,font=('Helvitica',20,'bold'),height=90,width=170,image=btn_image2,compound='left',command=window.quit)
-btn.grid(row=4,pady=(10,10),column=1)
+btn=Button(mainFrame,font=('Helvitica',20,'bold'),height=90,width=200,image=btn_image2,compound='left',command=window.quit)
+btn.grid(row=1,pady=(10,10),column=2)
 
 
 
--- record.py
+++ record.py
@@ -9,10 +9,10 @@ def record():
 
     while True:
         ret,frame=cap.read()
-        cv2.putText(frame,f'{datetime.now().strftime("%D-%H-%M-%S")}',(50,50),cv2.FONT_HERSHEY_COMPLEX,0.6,(255,255,255),2)
+        cv2.putText(frame,f'{datetime.now().strftime("%D-%H-%M-%S")}',(30,40),cv2.FONT_ITALIC,0.5,(255,255,255),2)
         storage.write(frame)
 
-        cv2.imshow('esc to stop',frame)
+        cv2.imshow('ESC to stop',frame)
 
         if cv2.waitKey(1)==27:
             cap.release()
